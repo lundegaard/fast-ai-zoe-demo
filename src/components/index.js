399553a691a5682c934778ba74edd885ee147fc2
Tomáš Konrády <tomaskonrady@gmail.com>
2020-04-08 16:09:57 +0200

    adding intl for Predictions modal

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
 export { default as ZoeLogo } from './ZoeLogo';
 export { default as DevConsole, useDevConsole, DevConsoleProvider } from './DevConsole';
+export { default as OptionalFormattedMessage } from './OptionalFormattedMessage';
+
 export * from './forms';
