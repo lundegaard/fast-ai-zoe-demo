ee406a7901774df3b2c6f700f03b442c8e028f98
Tomáš Konrády <tomaskonrady@gmail.com>
2020-08-05 11:28:11 +0200

    decoupling zoe-console

--- docker/image_build.sh
+++ docker/image_build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo "Building docker image for zone-demo"
+echo "Building docker image for zoe-demo"
 API_URL=http://localhost:8080/api/v1
 SA_DISTRIBUTION_URL=http://localhost:8081/versions/stable
 IMAGE_ID=docker.pkg.github.com/lundegaard/fast-ai-zoe-demo/zoe-demo
---
+++ packages/zoe-console/src/components/ZoeConsole.js
@@ -0,0 +1,79 @@
+import React, { useEffect, useMemo, useRef } from 'react';
+import PropTypes from 'prop-types';
+import {
+	Console,
+	ConsoleProvider,
+	defaultConsoleFormattingFunction,
+	useConsole,
+	useIdleTime,
+} from '@fast-ai/ui-components';
+import { useInterval } from '@restart/hooks';
+import { fetchFeatures } from '@fast-ai/zoe-client';
+
+import { createFeatureLog } from '../createFeatureLog';
+import { defaultDescriptor } from '../defaultDescriptor';
+
+const ZoeConsoleConsumer = ({
+	saRef: saRefProp,
+	applicationId,
+	loggingInterval = 2000,
+	descriptor = defaultDescriptor,
+	formatValue = defaultConsoleFormattingFunction,
+	title = 'zoe@lundegaard.ai:~ smartfeatures$',
+}) => {
+	const { isIdle } = useIdleTime();
+	const devConsole = useConsole();
+	const saRef = useRef();
+
+	saRef.current = (saRefProp && saRefProp.current) || window.sa;
+
+	useEffect(() => {
+		if (!saRef.current || !applicationId) {
+			return;
+		}
+		const { tid } = saRef.current('get', 'userInfo');
+
+		devConsole.replace({
+			'Application ID': applicationId,
+			'Tenant ID': tid,
+		});
+	}, [applicationId, devConsole, saRef]);
+
+	const logFeatures = useMemo(() => createFeatureLog(descriptor), [descriptor]);
+
+	useInterval(
+		() => {
+			if (document.hidden || isIdle || !applicationId) {
+				return;
+			}
+
+			fetchFeatures({ applicationId }).then(
+				(features) => void devConsole.log(logFeatures(features))
+			);
+		},
+		loggingInterval,
+		false, // is paused
+		true // run immediately
+	);
+
+	return <Console formatValue={formatValue} title={title} />;
+};
+
+ZoeConsoleConsumer.propTypes = {
+	applicationId: PropTypes.string,
+	descriptor: PropTypes.object,
+	formatValue: PropTypes.func,
+	loggingInterval: PropTypes.number,
+	saRef: PropTypes.shape({ current: PropTypes.object }),
+	title: PropTypes.node,
+};
+
+const ZoeConsole = (props) => (
+	<ConsoleProvider>
+		<ZoeConsoleConsumer {...props} />
+	</ConsoleProvider>
+);
+
+export default ZoeConsole;
+
+export { defaultConsoleFormattingFunction };
