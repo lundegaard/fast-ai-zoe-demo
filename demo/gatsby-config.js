2c59c9d940a91988e9fd2206a2ac22c36431dd15
Tomáš Konrády <tomaskonrady@gmail.com>
2020-08-07 15:14:04 +0200

    chore: config up a level

--- demo/gatsby-config.js
+++ demo/gatsby-config.js
@@ -3,7 +3,7 @@ const path = require('path');
 const { author } = require('./package.json');
 
 require('dotenv').config({
-	path: path.join(__dirname, `.env.${process.env.NODE_ENV}`),
+	path: path.join(__dirname, '..', `.env.${process.env.NODE_ENV}`),
 });
 
 const siteMetadata = {
