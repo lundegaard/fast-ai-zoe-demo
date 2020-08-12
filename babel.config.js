2230212b78b0f65934ed996a618e2b9bb244b4d1
Tomáš Konrády <tomaskonrady@gmail.com>
2020-08-12 11:19:13 +0200

    fix docker

---
+++ babel.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+module.exports = (api) => {
+	api.cache.never();
+
+	require('dotenv').config({
+		path: path.join(__dirname, `.env.${process.env.NODE_ENV}`),
+	});
+
+	return {
+		presets: [
+			[
+				'babel-preset-react-union',
+				{
+					test: process.env.NODE_ENV === 'test',
+					loose: true,
+					library: false,
+					universal: false,
+				},
+			],
+		],
+		plugins: [
+			[
+				'transform-inline-environment-variables',
+				{
+					include: [
+						'TENANT_ID',
+						'AUTH_DEMO_APP_TOKEN',
+						'API_URL',
+						'SA_DISTRIBUTION_URL',
+					],
+				},
+			],
+		],
+	};
+};
